b21f2156a20483dab9ca9056945d26a65bc385ad
leumars-o <samex150@gmail.com>
2024-07-26 02:51:08 +0100

    implementation of pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""This module defines a function: Pascal's Triangle
+"""
+
+def pascal_triangle(n):
+    """This function returns a list of lists of integers representing the Pascal's triangle of n
+        args:
+            n: an integer
+        return:
+            a list of lists of integers
+    """
+    # Handle the case when n is 0 or less
+    if n <= 0:
+        return [[]]
+
+    # Create an empty array to store the triangle
+    triangle = []
+
+    # create base case
+    rowOne = [1]
+    triangle.append(rowOne)
+
+    # create the rest of the rows 
+    for row in range(1, n):
+        # create an empty row
+        currentRow = []
+
+        # add the first element of the row
+        currentRow.append(1)
+
+        # add the rest of the elements
+        for i in range(1, row):
+            # Calculate the value of the row using the previous row
+            value = triangle[row - 1][i - 1] + triangle[row -1][i]
+            currentRow.append(value)
+        
+        # add the last element of the row
+        currentRow.append(1)
+
+        # add the row to the triangle
+        triangle.append(currentRow)
+    
+    # return the triangle
+    return triangle
